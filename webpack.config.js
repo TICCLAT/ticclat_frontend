1040348f19b6326a19b34bcbdc92d6cbc7b0b60e
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2019-06-26 14:52:48 +0200

    Added typescript, tslint and prettier setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,44 @@
-module.exports =
-    {
 
-        entry: './src/index.js',
-        output: { path: __dirname + '/dist', publicPath: '/', filename: 'bundle.js' },
-        devServer: { contentBase: './dist', },
-        devtool: 'inline-source-map',  //to generate source map
-        module: {
-            rules: [{
-                test: /\.(js|jsx)$/,
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+
+    // webpack will take the files from ./src/index
+    entry: './src/index',
+
+    // and output it into /dist as bundle.js
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+
+    module: {
+        rules: [
+
+            // we use babel-loader to load our jsx and tsx files
+            {
+                test: /\.(ts|js)x?$/,
                 exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            }]
-        },
-    };
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+
+            // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
