af74ad9594f45b39d03c73f0a7771b1d05608fdb
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2019-07-08 16:02:33 +0200

    change in name import

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Grid, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Timeline from '../components/Timeline';
+import Timeline from '../components/timeline';
 import clsx from 'clsx';
 const useStyles = makeStyles(theme => ({
     container: {
