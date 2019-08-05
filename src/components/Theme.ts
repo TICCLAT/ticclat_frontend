cf68d2e60b8d1786d8edbae44ef4b4cf3105b198
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2019-08-05 17:40:26 +0200

    Created SearchBar Component

--- src/components/Theme.ts
+++ src/components/Theme.ts
@@ -1,8 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import { common } from '@material-ui/core/colors';
 const theme = createMuiTheme({
     palette: {
         primary: {
             main: '#3F51B5'
+        },
+        common: {
+            black: '#000'
         }
     }
 })
