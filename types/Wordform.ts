add771003b81d57d065aac25ee4680044d1b3805
Maarten van Meersbergen <maartenvm@gmail.com>
2019-08-08 12:03:46 +0200

    added horizon chart types

--- types/Wordform.ts
+++ types/Wordform.ts
@@ -13,3 +13,42 @@ export interface IData {
     metadata: IMetadata,
     wordform: string
 }
+
+export interface ICorpusFrequencyEntry {
+    freq: number,
+    year: ''
+}
+
+export interface ICorpus {
+    frequencies: Array<ICorpusFrequencyEntry>,
+    name: ''
+}
+
+export interface IVariant {
+    V: 1,
+    corpora: Array<ICorpus>,
+    word_type_code: string,
+    word_type_number: number,
+    wordform: string
+}
+
+export interface ILemma {
+    lemma: string,
+    paradigm_code: string,
+    variants: Array<IVariant>
+}
+
+export interface IVariantsMetadata {
+    max_freq: number,
+    max_year: number,
+    min_freq: number,
+    min_year: number,
+    overall_max_year: number,
+    overall_min_year: number
+}
+
+export interface IVariantsQueryData {
+    metadata: IVariantsMetadata,
+    paradigms: Array<ILemma>,
+    wordform: string
+}
