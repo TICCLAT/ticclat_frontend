a375e9a31d589ade56874b79a4b16de618148211
Tom Klaver <tomklav@gmail.com>
2019-10-28 11:29:09 +0100

    fix DatabaseOverview, remove title

--- src/components/DatabaseOverview.tsx
+++ src/components/DatabaseOverview.tsx
@@ -4,64 +4,64 @@ import { makeStyles } from '@material-ui/core/styles';
 import BokehPlot from '../components/BokehPlot';
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    plotContainerSmall: {
-        minHeight: 300,
-    },
-    plotContainerBig: {
-        minHeight: 600,
-    },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  plotContainerSmall: {
+    minHeight: 300,
+  },
+  plotContainerBig: {
+    minHeight: 600,
+  },
 
 }));
 
 const DatabaseOverview = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            <p>These graphs give an overview of the current contents of this TICCLAT database. The database is populated using three main sources: lexica (e.g. dictionaries), corpora (books, newspapers, etc.) and morphological paradigms.</p>
-            <p>Top left: lexicon sizes in number of wordforms.</p>
-            <p>Top right: corpus sizes in number of wordforms.</p>
-            <p>Second row: number of wordforms in the different corpora.</p>
-            <p>Bottom row: number wordforms with certain paradigm codes. See the glossary for more information on the codes.</p>
-        </div>
-        <Container maxWidth="lg" className={classes.container}>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={6} className={classes.plotContainerSmall}>
-                    <BokehPlot source="/plots/lexicon_size" />
-                </Grid>
-                <Grid item xs={12} md={6} className={classes.plotContainerSmall}>
-                    <BokehPlot source="/plots/corpus_size" />
-                </Grid>
-                <Grid item xs={12} className={classes.plotContainerBig}>
-                    <BokehPlot source="/plots/word_count_per_year" />
-                </Grid>
-                <Grid item xs={12} className={classes.plotContainerBig}>
-                    <Paper>
-                        <Grid container>
-                            <Grid item xs={4} className={classes.plotContainerSmall}>
-                                <BokehPlot source="/plots/paradigm_size?var=X" />
-                            </Grid>
-                            <Grid item xs={4} className={classes.plotContainerSmall}>
-                                <BokehPlot source="/plots/paradigm_size?var=Y" />
-                            </Grid>
-                            <Grid item xs={4} className={classes.plotContainerSmall}>
-                                <BokehPlot source="/plots/paradigm_size?var=Z" />
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                </Grid>
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+      <div>
+        <p>These graphs give an overview of the current contents of this TICCLAT database. The database is populated using three main sources: lexica (e.g. dictionaries), corpora (books, newspapers, etc.) and morphological paradigms.</p>
+        <p>Top left: lexicon sizes in number of wordforms.</p>
+        <p>Top right: corpus sizes in number of wordforms.</p>
+        <p>Second row: number of wordforms in the different corpora.</p>
+        <p>Bottom row: number wordforms with certain paradigm codes. See the glossary for more information on the codes.</p>
+      </div>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} className={classes.plotContainerSmall}>
+          <BokehPlot source="/plots/lexicon_size" />
+        </Grid>
+        <Grid item xs={12} md={6} className={classes.plotContainerSmall}>
+          <BokehPlot source="/plots/corpus_size" />
+        </Grid>
+        <Grid item xs={12} className={classes.plotContainerBig}>
+          <BokehPlot source="/plots/word_count_per_year" />
+        </Grid>
+        <Grid item xs={12} className={classes.plotContainerBig}>
+          <Paper>
+            <Grid container>
+              <Grid item xs={4} className={classes.plotContainerSmall}>
+                <BokehPlot source="/plots/paradigm_size?var=X" />
+              </Grid>
+              <Grid item xs={4} className={classes.plotContainerSmall}>
+                <BokehPlot source="/plots/paradigm_size?var=Y" />
+              </Grid>
+              <Grid item xs={4} className={classes.plotContainerSmall}>
+                <BokehPlot source="/plots/paradigm_size?var=Z" />
+              </Grid>
             </Grid>
-        </Container>
-    )
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  )
 }
 
 export default DatabaseOverview;
--- src/components/OverviewTabs.tsx
+++ src/components/OverviewTabs.tsx
@@ -12,164 +12,164 @@ import { Info } from '@material-ui/icons';
 import ParadigmNetwork from '../pages/ParadigmNetwork';
 
 interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
+  children?: React.ReactNode;
+  index: any;
+  value: any;
 }
 
 // Header Component for Tab Panel
 const Header = ({ title, section }: { title: string, section: string }) => {
-    return (
-        <div style={{ display: 'flex' }}>
-            <Typography variant="h6" id="tableTitle" align='center' style={{ flex: 8 }}>
-                {title}
-            </Typography>
-            <IconButton color="primary" href={"/glossary#" + section}><Info /></IconButton>
-        </div>
-    )
+  return (
+    <div style={{ display: 'flex' }}>
+      <Typography variant="h6" id="tableTitle" align='center' style={{ flex: 8 }}>
+        {title}
+      </Typography>
+      <IconButton color="primary" href={"/glossary#" + section}><Info /></IconButton>
+    </div>
+  )
 }
 
 // TabPanel Component to display tab specific data
 function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            id={`wrapped-tabpanel-${index}`}
-            aria-labelledby={`wrapped-tab-${index}`}
-            style={{ margin: 20 }}
-            {...other}
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
+      style={{ margin: 20 }}
+      {...other}
 
-        >
-            <Grid container={true} spacing={3}>
-                {children}
-            </Grid>
-        </Typography>
-    );
+    >
+      <Grid container={true} spacing={3}>
+        {children}
+      </Grid>
+    </Typography>
+  );
 }
 
 interface IProps {
-    searchValue: string;
-    onSearch: (searchValue: string) => void
+  searchValue: string;
+  onSearch: (searchValue: string) => void
 }
 const OverviewTabs = (props: IProps) => {
-    // declare state to store selected tab value
-    const [selectedTab, setSelectedTab] = React.useState(0);
-    // props destructuring
-    const { searchValue, onSearch } = props;
-    // Function to set newly selcted tab
-    const handleChange = (event: React.ChangeEvent<{}>, newTab: number) => {
-        setSelectedTab(newTab);
-    };
+  // declare state to store selected tab value
+  const [selectedTab, setSelectedTab] = React.useState(0);
+  // props destructuring
+  const { searchValue, onSearch } = props;
+  // Function to set newly selcted tab
+  const handleChange = (event: React.ChangeEvent<{}>, newTab: number) => {
+    setSelectedTab(newTab);
+  };
 
-    return (
-        <>
-            <SearchBar onSearch={onSearch} wordform={searchValue} />
-            <Tabs
-                value={selectedTab}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Word usage over time" />
-                <Tab label="Paradigms Table" />
-                <Tab label="Paradigms over time" />
-                <Tab label="OCR Postcorrection view" />
-                <Tab label="Paradigm network" />
-                <Tab label="Database Overview" />
-            </Tabs>
+  return (
+    <>
+      <SearchBar onSearch={onSearch} wordform={searchValue} />
+      <Tabs
+        value={selectedTab}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Word usage over time" />
+        <Tab label="Paradigms Table" />
+        <Tab label="Paradigms over time" />
+        <Tab label="OCR Postcorrection view" />
+        <Tab label="Paradigm network" />
+        <Tab label="Database Overview" />
+      </Tabs>
 
-            {selectedTab === 0 && (
-                <TabPanel value={selectedTab} index="one">
+      {selectedTab === 0 && (
+        <TabPanel value={selectedTab} index="one">
 
-                    <Grid item={true} xs={12} md={8} lg={8}>
-                        <Paper >
-                            <Header title={searchValue} section="ngram" />
-                            <NGramTimeline wordform={searchValue} />
-                        </Paper>
-                    </Grid>
-                    <Grid item={true} xs={12} md={4} lg={4}>
-                        <Paper>
-                            {searchValue !== '' ? <Lexica wordform={searchValue} /> : <CircularProgress />}
-                        </Paper>
-                    </Grid>
-                </TabPanel>
+          <Grid item={true} xs={12} md={8} lg={8}>
+            <Paper >
+              <Header title={searchValue} section="ngram" />
+              <NGramTimeline wordform={searchValue} />
+            </Paper>
+          </Grid>
+          <Grid item={true} xs={12} md={4} lg={4}>
+            <Paper>
+              {searchValue !== '' ? <Lexica wordform={searchValue} /> : <CircularProgress />}
+            </Paper>
+          </Grid>
+        </TabPanel>
 
-            )
-            }
-            {
-                selectedTab === 1 && (
-                    <TabPanel value={selectedTab} index="two">
+      )
+      }
+      {
+        selectedTab === 1 && (
+          <TabPanel value={selectedTab} index="two">
 
-                        <Grid item={true} xs={12} md={12} lg={12}>
-                            <Paper>
-                                <Header title={searchValue} section="paradigm" />
-                                {searchValue !== '' ? <Paradigms wordform={searchValue} /> : null}
-                            </Paper>
-                        </Grid>
-                    </TabPanel>
+            <Grid item={true} xs={12} md={12} lg={12}>
+              <Paper>
+                <Header title={searchValue} section="paradigm" />
+                {searchValue !== '' ? <Paradigms wordform={searchValue} /> : null}
+              </Paper>
+            </Grid>
+          </TabPanel>
 
-                )
-            }
-            {
-                selectedTab === 2 && (
-                    <TabPanel value={selectedTab} index="three">
+        )
+      }
+      {
+        selectedTab === 2 && (
+          <TabPanel value={selectedTab} index="three">
 
-                        <Grid item={true} xs={12} md={12} lg={12}>
-                            <Paper >
-                                <Header title={searchValue} section="Paradigms over time" />
-                                <HorizonChartContainer wordform={searchValue} />
-                            </Paper>
-                        </Grid>
-                    </TabPanel>
+            <Grid item={true} xs={12} md={12} lg={12}>
+              <Paper >
+                <Header title={searchValue} section="Paradigms over time" />
+                <HorizonChartContainer wordform={searchValue} />
+              </Paper>
+            </Grid>
+          </TabPanel>
 
-                )
-            }
-            {
-                selectedTab === 3 && (
-                    <TabPanel value={selectedTab} index="four">
+        )
+      }
+      {
+        selectedTab === 3 && (
+          <TabPanel value={selectedTab} index="four">
 
-                        <Grid item={true} xs={12} md={12} lg={12}>
-                            <Paper >
-                                <Header title={searchValue} section="OCR Postcorrection view" />
-                                <OCRPostcorrectionChartContainer wordform={searchValue} />
-                            </Paper>
-                        </Grid>
-                    </TabPanel>
-                )
-            }
-          {
-            selectedTab === 4 && (
-              <TabPanel value={selectedTab} index="five">
+            <Grid item={true} xs={12} md={12} lg={12}>
+              <Paper >
+                <Header title={searchValue} section="OCR Postcorrection view" />
+                <OCRPostcorrectionChartContainer wordform={searchValue} />
+              </Paper>
+            </Grid>
+          </TabPanel>
+        )
+      }
+      {
+        selectedTab === 4 && (
+          <TabPanel value={selectedTab} index="five">
 
-                <Grid item={true} xs={12} md={12} lg={12}>
-                  <Paper >
-                    <Header title="" section="Paradigm network" />
-                    <ParadigmNetwork />
-                  </Paper>
-                </Grid>
-              </TabPanel>
-            )
-          }
-            {
-                selectedTab === 5 && (
-                    <TabPanel value={selectedTab} index="six">
+            <Grid item={true} xs={12} md={12} lg={12}>
+              <Paper >
+                <Header title="" section="Paradigm network" />
+                <ParadigmNetwork />
+              </Paper>
+            </Grid>
+          </TabPanel>
+        )
+      }
+      {
+        selectedTab === 5 && (
+          <TabPanel value={selectedTab} index="six">
 
-                        <Grid item={true} xs={12} md={12} lg={12}>
-                            <Paper >
-                                <Header title={searchValue} section="Database overview" />
-                                <DatabaseOverview />
-                            </Paper>
-                        </Grid>
-                    </TabPanel>
-                )
-            }
+            <Grid item={true} xs={12} md={12} lg={12}>
+              <Paper >
+                <Header title="" section="Database overview" />
+                <DatabaseOverview />
+              </Paper>
+            </Grid>
+          </TabPanel>
+        )
+      }
 
-        </>
+    </>
 
-    );
+  );
 }
 export default OverviewTabs;
